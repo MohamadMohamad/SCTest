01b53044e1ed58bb03b2fbb12efa770431d2d9c6
Shawkey <mohamad.shawkey@gmail.com>
2017-04-17 18:48:34 +0200

    added first database class "Author"

---
+++ Model/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Starcounter;
+
+namespace SCTest
+{
+    [Database]
+    public class Author
+    {
+        public string FirstName;
+        public string LastName;
+    }
+}
